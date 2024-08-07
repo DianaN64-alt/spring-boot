00108e5368b9adf185fbba2f8cc1d15b743005ee
DianaN64-alt <diananursubina64@gmail.com>
2024-08-07 12:28:59 +0100

    Add files via upload

---
+++ demo/src/main/java/ru/kors/springstudents/service/Impl/StudentServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.kors.springstudents.service.Impl;
+
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kors.springstudents.model.Student;
+import ru.kors.springstudents.repository.StudentRepository;
+import ru.kors.springstudents.service.StudentService;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Primary
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public List<Student> findAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return studentRepository.findByEmail(email);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudent(String email) {
+        studentRepository.deleteByEmail(email);
+    }
+}
---
+++ demo/src/test/java/ru/kors/springstudents/demo/SpringStudentsApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.kors.springstudents.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
